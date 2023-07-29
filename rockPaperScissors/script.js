e397f06045d906eaf06e16503655e4f2aa840fd4
Sean Connolly <121255377+seanc00@users.noreply.github.com>
2023-07-29 19:34:46 +0100

    create js file, moved js code into js file

---
+++ rockPaperScissors/script.js
@@ -0,0 +1,87 @@
+function chooseRock() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+  // code to randomise computer move
+  if (randomNumber >= 0 && randomNumber <= 1/3) {
+    computerMove = 'Rock';
+  } else if (randomNumber > 1/3 && randomNumber <= 2/3) {
+    computerMove = 'Scissors';
+  } else  if (randomNumber > 2/3 && randomNumber < 1) {
+    computerMove = 'Paper';
+  }
+  // check to see if code works
+  //console.log(computerMove);
+
+  let result = '';
+  //code to determine winner
+  if (computerMove === 'Rock') {
+    result = 'Draw';
+  } else if (computerMove === 'Paper') {
+    result = 'You Lose!'
+  } else if (computerMove === 'Scissors') {
+    result = 'You Win!'
+  }
+  // check code to see if works
+  //console.log(result);
+  
+  computerMove.toUpperCase;
+  alert(`You picked Rock, Computer picked ${computerMove}. ${result}`);
+}
+
+function choosePaper() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+  // code to randomise computer move
+  if (randomNumber >= 0 && randomNumber <= 1/3) {
+    computerMove = 'Rock';
+  } else if (randomNumber > 1/3 && randomNumber <= 2/3) {
+    computerMove = 'Scissors';
+  } else  if (randomNumber > 2/3 && randomNumber < 1) {
+    computerMove = 'Paper';
+  }
+  // check to see if code works
+  //console.log(computerMove);
+
+  let result = '';
+  //code to determine winner
+  if (computerMove === 'Rock') {
+    result = 'You Win!';
+  } else if (computerMove === 'Paper') {
+    result = 'Draw!'
+  } else if (computerMove === 'Scissors') {
+    result = 'You Lose!'
+  }
+  // check code to see if works
+  //console.log(result);
+  computerMove.toUpperCase;
+  alert(`You picked Paper, Computer picked ${computerMove}. ${result}`);
+}
+
+function chooseScissors() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+  // code to randomise computer move
+  if (randomNumber >= 0 && randomNumber <= 1/3) {
+    computerMove = 'Rock';
+  } else if (randomNumber > 1/3 && randomNumber <= 2/3) {
+    computerMove = 'Scissors';
+  } else  if (randomNumber > 2/3 && randomNumber < 1) {
+    computerMove = 'Paper';
+  }
+  // check to see if code works
+  //console.log(computerMove);
+
+  let result = '';
+  //code to determine winner
+  if (computerMove === 'Rock') {
+    result = 'You Lose';
+  } else if (computerMove === 'Paper') {
+    result = 'You Win!'
+  } else if (computerMove === 'Scissors') {
+    result = 'Draw!'
+  }
+  // check code to see if works
+  //console.log(result);
+
+  alert(`You picked Scissors, Computer picked ${computerMove}. ${result}`);
+}
